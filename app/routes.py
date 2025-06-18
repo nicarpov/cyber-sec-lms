037400f6331c629585fc683c9a2f5338a7e33804
Nikita Arapov <nicarpov@gmail.com>
2025-06-18 16:38:45 +0300

    labs template added

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,16 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/labs')
+def labs():
+    lab = {
+        "id": 1,
+        "name": "ЛР1.1",
+    }
+    return render_template('labs.html', lab=lab, form=form)
+
+@app.route('/lab_manual/<lab_id>')
+def lab_manual(lab_id):
+
+    return render_template('lab_manual.html', manual_link=link)
